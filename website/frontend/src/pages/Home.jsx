c1a8925055e79f3bd5036be7af72ff16a1ee95cb
Emmanuel <emmanuel@cloudadventures.net>
2025-11-20 11:07:46 -0500

    Uploading Website Back&Front-end Scaffolding

---
+++ website/frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import Navbar from "../components/Navbar.jsx";
+//import { useEffect, useState } from "react";
+//import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+
+   return (
+      <main>
+         <Navbar />
+         <div style={styles.body}>
+            <h2>Stuff</h2>
+         </div>
+
+      </main>
+   )
+};
+
+const styles = {
+   body: {
+      marginTop: '15vh',
+   }
+}
+
+export default Home;
