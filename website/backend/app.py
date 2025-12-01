94043a42466840258a2190063a2ca95da1b2edd2
Emmanuel <emmanuel@cloudadventures.net>
2025-12-01 14:14:59 -0800

    Corrected Cors + Adjusted Auth Routes

--- website/backend/app.py
+++ website/backend/app.py
@@ -7,19 +7,24 @@ def create_app():
     load_dotenv()
 
     app = Flask(__name__)
-    CORS(app)
 
-    load_dotenv()  # Load .env file
+    CORS(app, supports_credentials=True)
 
+    # Secret key
+    global SECRET_KEY
     SECRET_KEY = os.getenv("SECRET_KEY")
+    app.config["SECRET_KEY"] = SECRET_KEY
 
     from routes import cards_bp, auth
+
     app.register_blueprint(cards_bp, url_prefix="/api/cards")
-    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(auth, url_prefix="/api")
 
     return app
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+
