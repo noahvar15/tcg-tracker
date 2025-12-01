8b11eb8cc125958dfd4883de9b845528c4ea6680
Emmanuel <emmanuel@cloudadventures.net>
2025-12-01 14:16:52 -0800

    Routes for Login/Signup Pages

--- website/frontend/src/App.jsx
+++ website/frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx'
+import SignUp from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/Signup' element={<SignUp />} />
+        <Route path='/Login' element={<Login />} />
       </Routes>
     </Router>
   )
