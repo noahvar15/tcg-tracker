3f7711cdd0edf31fa8ba6cdb2583a49b205280e5
Joshua Pereira <jmp511@outlook.com>
2025-12-01 20:38:06 -0500

    fixed rendering issue

--- website/frontend/src/pages/PokemonCardsBySets.jsx
+++ website/frontend/src/pages/PokemonCardsBySets.jsx
@@ -1,18 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 export default function PokemonCardsBySets() {
   const { setId } = useParams();
 
   // Example: fetch cards
   useEffect(() => {
-    fetch(`http://localhost:8000/api/cards/pokemon/cards/${setId}`)
+    fetch(`http://127.0.0.1:5000/api/cards/pokemon/cards/${setId}`)
       .then(res => res.json())
       .then(data => console.log(data));
   }, [setId]);
 
   return (
-    <div>
-      <h1>Cards for Set: {setId}</h1>
-    </div>
+    <main>
+        <Navbar />
+        <div style={styles.body}>
+            <h1>Cards for Set: {setId}</h1>
+        </div>
+    </main>
   );
 }
+
+const styles = {
+    body: {
+       marginTop: '15vh',
+    }
+ }
